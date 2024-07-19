75c128447c8f18c76168e06e79a8ca48169a4890
axl214v <axl214v@gmail.com>
2024-07-19 22:14:09 +0300

    Conf added new files

--- Conf/conf.php
+++ Conf/conf.php
@@ -1,12 +1,16 @@
+<?php
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="conf.css">
     <title>Meetify</title>
     <h1>Создайте конференцию</h1>
 </head>
 <body>
-    
+    <script src="conf.js"></script>
 </body>
 </html>
