d5c785b5ce9b79c9f91726313bcfa9f63bccd36e
axl214v <axl214v@gmail.com>
2026-03-14 00:20:47 +0300

    What's changed
    Fixed a big problem with backend, now it working and responding on all the requests

--- src/server_js/config/config.js
+++ src/server_js/config/config.js
@@ -3,7 +3,7 @@
 const config = {
   // База данных
   database: {
-    host: process.env.DB_HOST || 'localhost',
+    host: process.env.DB_HOST || '0.0.0.0',
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '',
     database: process.env.DB_NAME || 'meetify',
