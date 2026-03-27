a49154c438b71197c4b9a2cdbfa91a55718bdc75
axl214v <axl214v@gmail.com>
2026-03-27 23:28:29 +0300

    What's changed
    Added to models of user new functions that will help update/delete users from database

--- src/server_js/models/User.js
+++ src/server_js/models/User.js
@@ -28,6 +28,34 @@ const User = {
                 resolve({ id: results.insertId, ...userData });
             });
         });
+    },
+    update: (id, data) => {
+        return new Promise((resolve, reject) => {
+            const fields = Object.keys(data)
+                .map(key => `${key} = ?`)
+                .join(', ');
+            const values = [...Object.values(data), id];
+
+            const query = `UPDATE users SET ${fields} WHERE id = ?`;
+
+            db.query(query, values, (error, result) => {
+                if (error) return reject(error);
+                // возвращаем обновлённого юзера
+                db.query('SELECT * FROM users WHERE id = ?', [id], (err, rows) => {
+                    if (err) return reject(err);
+                    resolve(rows[0]);
+                });
+            });
+        });
+    },
+
+    delete: (id) => {
+        return new Promise((resolve, reject) => {
+            db.query('DELETE FROM users WHERE id = ?', [id], (error) => {
+                if (error) return reject(error);
+                resolve(true);
+            });
+        });
     }
 };
 
