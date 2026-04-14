16705f0cbc05e83c0b666f845c8cdd9212a2f0c8
axl214v <axl214v@gmail.com>
2026-04-14 12:30:45 +0300

    What's changd
    Fixed little bug with requirements

--- src/server_js/services/authService.js
+++ src/server_js/services/authService.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const config = require('../config/config');
 const UserService = require('./userService');
+const db = require('../config/database');
 
 class AuthService {
   // Регистрация нового пользователя
